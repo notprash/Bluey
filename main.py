4038d30fcb170f591e1d0be1bfdd05ae9f6f777b
PrashantBaid11 <prashantbaid80@gmail.com>
2021-03-27 13:01:03 +0530

    Added welcome message feature

--- main.py
+++ main.py
@@ -5,19 +5,10 @@ import json
 import random
 
 
-client = commands.Bot(command_prefix='*')
+intents = discord.Intents.default()
+intents.members = True
 
-
-@client.event
-async def on_guild_join(guild):
-    with open('Data/data.json', 'r') as f:
-        guild_info = json.load(f)
-
-    guild_info[guild.text_channels[0].id] = str(guild.text_channels[0])
-    print(guild.text_channels[0])
-
-    with open("Data/data.json", "w") as f:
-        json.dump(guild_info, f)
+client = commands.Bot(command_prefix='*', intents=intents)
 
 
 @client.event
@@ -26,7 +17,47 @@ async def on_ready():
 
 
 @client.event
+async def on_guild_join(guild):
+    with open('data.json', 'w') as f:
+        f.write('{}')
+    with open('data.json', 'r') as f:
+        guild_info = json.load(f)
+
+    guild_info[str(guild.id)] = (guild.text_channels[0].id)
+    with open("data.json", "w") as f:
+        json.dump(guild_info, f)
+
+
+@client.command()
+async def changewelcomechannel(ctx, arg):
+    with open("data.json", "r") as f:
+        guildInfo = json.load(f)
+
+    guildInfo[str(ctx.message.guild.id)] = ctx.message.channel.id
+
+    with open("data.json", "w") as f:
+        json.dump(guildInfo, f)
+
+    await ctx.message.channel.send(
+        f"Welcome Channel has been changed to `{arg}`")
+
+
+@client.event
 async def on_member_join(member):
-    pass
+    colour = discord.Color
+    colours = [colour.teal(), colour.green(),
+               colour.orange(), colour.blue()]
+
+    embed = discord.Embed(
+        title=f"Info", colour=random.choice(colours))
+    embed.add_field(name="Name", value=member.name, inline=True)
+    embed.add_field(name="Status", value=member.status, inline=True)
+    embed.add_field(name="Roles", value=member.top_role, inline=False)
+    embed.set_thumbnail(url=member.avatar_url)
+
+    with open('data.json', 'r') as f:
+        guildInfo = json.load(f)
+
+    channel_id = guildInfo[str(member.guild.id)]
+    await client.get_channel(channel_id).send(f'Hey {member.mention}, Welcome to {member.guild.name}', embed=embed)
 client.run(config('TOKEN'))
-print(client.guilds[0].text_channels[0].id)
