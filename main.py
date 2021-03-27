00918129343e69107291712725bd53095914ff06
PrashantBaid11 <prashantbaid80@gmail.com>
2021-03-27 19:34:50 +0530

    Added Bye Message feature

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ intents.members = True
 client = commands.Bot(command_prefix='*', intents=intents)
 
 
+def load_data_json():
+    with open('data.json', 'r') as f:
+        guildInfo = json.load(f)
+
+    return guildInfo
+
+
 @client.event
 async def on_ready():
     print("Bot is ready")
@@ -20,22 +27,23 @@ async def on_ready():
 async def on_guild_join(guild):
     with open('data.json', 'w') as f:
         f.write('{}')
-    with open('data.json', 'r') as f:
-        guild_info = json.load(f)
 
-    guild_info[str(guild.id)] = (guild.text_channels[0].id)
+    guild_info = load_data_json()
+
+    guild_info[str(guild.id)] = {
+        'welcome': guild.text_channels[0].id, 'bye': guild.text_channels[0].id}
     with open("data.json", "w") as f:
         json.dump(guild_info, f)
 
 
 @client.command()
 async def welcomeChannel(ctx, arg):
-    with open("data.json", "r") as f:
-        guildInfo = json.load(f)
 
-    if ctx.message.author.server_permissions.administrator:
+    guildInfo = load_data_json()
 
-        guildInfo[str(ctx.message.guild.id)] = discord.utils.get(
+    if ctx.message.author.guild_permissions.administrator:
+
+        guildInfo[str(ctx.message.guild.id)]['welcome'] = discord.utils.get(
             ctx.guild.channels, name=arg).id
 
         with open("data.json", "w") as f:
@@ -58,9 +66,33 @@ async def on_member_join(member):
     embed.add_field(name="Roles", value=member.top_role, inline=False)
     embed.set_thumbnail(url=member.avatar_url)
 
-    with open('data.json', 'r') as f:
-        guildInfo = json.load(f)
+    guildInfo = load_data_json()
 
-    channel_id = guildInfo[str(member.guild.id)]
+    channel_id = guildInfo[str(member.guild.id)]['welcome']
     await client.get_channel(channel_id).send(f'Hey {member.mention}, Welcome to {member.guild.name}', embed=embed)
+
+
+@client.command()
+async def byeChannel(ctx, arg):
+    guildInfo = load_data_json()
+
+    if ctx.message.author.guild_permissions.administrator:
+
+        guildInfo[str(ctx.message.guild.id)]['bye'] = discord.utils.get(
+            ctx.guild.channels, name=arg).id
+
+        with open("data.json", "w") as f:
+            json.dump(guildInfo, f)
+
+        await ctx.message.channel.send(
+            f"Welcome Channel has been changed to `{arg}`")
+
+
+@client.event
+async def on_member_remove(member):
+    guildInfo = load_data_json()
+
+    channel_id = guildInfo[str(member.guild.id)]['bye']
+    await client.get_channel(channel_id).send(f'{member.name} just left the server.')
+
 client.run(config('TOKEN'))
