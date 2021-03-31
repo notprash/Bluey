da724003b901f77fbb7148d96b413bfb86108856
PrashantBaid11 <prashantbaid80@gmail.com>
2021-03-31 18:13:53 +0530

    Added some commands

--- main.py
+++ main.py
@@ -18,6 +18,20 @@ def load_json(file):
     return guildInfo
 
 
+def str_to_bool(string):
+    result = ''
+    if string == 'True':
+        result = True
+    elif string == 'False':
+        result = False
+
+    return result
+
+
+def has_admin_permissions():
+    return commands.has_permissions(administrator=True)
+
+
 @client.event
 async def on_ready():
     print("Bot is ready")
@@ -61,53 +75,6 @@ async def on_guild_remove(guild):
         json.dump(data, f)
 
 
-def admin(ctx):
-    return ctx.message.author.guild_permissions.administrator
-
-
-@client.command()
-async def welcomeChannel(ctx, arg):
-
-    guildInfo = load_json('data.json')
-
-    if admin(ctx):
-
-        guildInfo[str(ctx.message.guild.id)]['welcome'] = discord.utils.get(
-            ctx.guild.channels, name=arg).id
-
-        with open("data.json", "w") as f:
-            json.dump(guildInfo, f)
-
-        await ctx.message.channel.send(
-            f"Welcome Channel has been changed to `{arg}`")
-
-
-def str_to_bool(string):
-    result = ''
-    if string == 'True':
-        result = True
-    elif string == 'False':
-        result = False
-
-    return result
-
-
-@client.command()
-async def welcomeMsg(ctx, arg):
-    settings = load_json('settings.json')
-    if arg.lower() == 'enable':
-        settings[str(ctx.message.guild.id)]['welcome'] = 'True'
-        await ctx.message.channel.send('Welcome Message is enabled')
-    elif arg.lower() == 'disable':
-        settings[str(ctx.message.guild.id)]['welcome'] = 'False'
-        await ctx.message.channel.send('Welcome Message is disabled')
-    else:
-        await ctx.message.channel.send("Could not understand that")
-
-    with open('settings.json', 'w') as file:
-        json.dump(settings, file)
-
-
 @client.event
 async def on_member_join(member):
     settings = load_json('settings.json')
@@ -123,26 +90,99 @@ async def on_member_join(member):
         guildInfo = load_json('data.json')
 
         channel_id = guildInfo[str(member.guild.id)]['welcome']
+
         await client.get_channel(channel_id).send(f'Hey {member.mention}, Welcome to {member.guild.name}', embed=embed)
 
 
+@client.event
+async def on_member_remove(member):
+    settings = load_json('settings.json')
+    enabled = str_to_bool(settings[str(member.guild.id)]['bye'])
+    if enabled:
+        if member.id != client.user.id:
+            guildInfo = load_json('data.json')
+
+            channel_id = guildInfo[str(member.guild.id)]['bye']
+            await client.get_channel(channel_id).send(f'{member.name} just left the server.')
+
+
 @client.command()
+@has_admin_permissions()
+async def welcomeChannel(ctx, arg):
+
+    guildInfo = load_json('data.json')
+
+    guildInfo[str(ctx.message.guild.id)]['welcome'] = discord.utils.get(
+        ctx.guild.channels, name=arg).id
+
+    with open("data.json", "w") as f:
+        json.dump(guildInfo, f)
+
+    await ctx.message.channel.send(
+        f"Welcome Channel has been changed to `{arg}`")
+
+
+@client.command()
+@has_admin_permissions()
+async def welcomeMsg(ctx, arg):
+    settings = load_json('settings.json')
+    if arg.lower() == 'enable':
+        settings[str(ctx.message.guild.id)]['welcome'] = 'True'
+        await ctx.message.channel.send('Welcome Message is enabled')
+    elif arg.lower() == 'disable':
+        settings[str(ctx.message.guild.id)]['welcome'] = 'False'
+        await ctx.message.channel.send('Welcome Message is disabled')
+    else:
+        await ctx.message.channel.send("Could not understand that")
+
+    with open('settings.json', 'w') as file:
+        json.dump(settings, file)
+
+
+@client.command()
+@has_admin_permissions()
 async def userLeaveChannel(ctx, arg):
     guildInfo = load_json('data.json')
 
-    if admin(ctx):
+    guildInfo[str(ctx.message.guild.id)]['bye'] = discord.utils.get(
+        ctx.guild.channels, name=arg).id
 
-        guildInfo[str(ctx.message.guild.id)]['bye'] = discord.utils.get(
-            ctx.guild.channels, name=arg).id
+    with open("data.json", "w") as f:
+        json.dump(guildInfo, f)
 
-        with open("data.json", "w") as f:
-            json.dump(guildInfo, f)
-
-        await ctx.message.channel.send(
-            f"UserLeave Channel has been changed to `{arg}`")
+    await ctx.message.channel.send(
+        f"UserLeave Channel has been changed to `{arg}`")
 
 
 @client.command()
+async def avatar(ctx, member: discord.User = None):
+    id = member.id
+    avatar = client.get_user(id).avatar_url
+    await ctx.send(avatar)
+
+
+@client.command()
+@commands.has_permissions(manage_channels=True)
+async def lock(ctx, channel: discord.TextChannel = None):
+    channel = channel or ctx.channel
+    overwrite = channel.overwrites_for(ctx.guild.default_role)
+    overwrite.send_messages = False
+    await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+    await ctx.send('Channel locked.')
+
+
+@client.command()
+@has_admin_permissions()
+async def unlock(ctx, channel: discord.TextChannel = None):
+    channel = channel or ctx.channel
+    overwrite = channel.overwrites_for(ctx.guild.default_role)
+    overwrite.send_messages = True
+    await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+    await ctx.send('Channel Unlocked.')
+
+
+@client.command()
+@has_admin_permissions()
 async def userLeave(ctx, arg):
     settings = load_json('settings.json')
     if arg.lower() == 'enable':
@@ -156,16 +196,5 @@ async def userLeave(ctx, arg):
     with open('settings.json', 'w') as file:
         json.dump(settings, file)
 
-
-@client.event
-async def on_member_remove(member):
-    settings = load_json('settings.json')
-    enabled = str_to_bool(settings[str(member.guild.id)]['bye'])
-    if enabled:
-        if member.id != client.user.id:
-            guildInfo = load_json('data.json')
-
-            channel_id = guildInfo[str(member.guild.id)]['bye']
-            await client.get_channel(channel_id).send(f'{member.name} just left the server.')
 
 client.run(config('TOKEN'))
