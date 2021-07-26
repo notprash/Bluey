f1decbbe8b87535a4699f99cfd88fa8acb337f73
E-M-P-E-R-O-R <70442265+E-M-P-E-R-O-R@users.noreply.github.com>
2021-07-26 20:36:32 +0530

    Update animal.py

--- src/animal.py
+++ src/animal.py
@@ -5,10 +5,10 @@ from discord.embeds import Embed
 from requests import get
 
 # Links for API Call
-dkey = 'https://dog.ceo/api/breeds/image/random'
-ckey = 'https://api.thecatapi.com/v1/images/search'
-pkey = 'https://some-random-api.ml/img/panda'
-rpkey = 'https://some-random-api.ml/img/red_panda'
+dkey = 'https://dog.ceo/api/breeds/image/random'  # Dog API
+ckey = 'https://api.thecatapi.com/v1/images/search' # Cat API
+pkey = 'https://some-random-api.ml/img/panda' #Panda API
+rpkey = 'https://some-random-api.ml/img/red_panda' #Red Panda API
 
 
 class Animal(commands.Cog):
@@ -18,10 +18,10 @@ class Animal(commands.Cog):
 #### Command for Dog pics
 
     @commands.command()
-    async def dog(self, ctx): 
+    async def dog(self, ctx):
         data = get(dkey).json()
         dog = data['message']
-        emb = discord.Embed(title = 'Dogs') 
+        emb = discord.Embed(title = 'Dogs', color = discord.Color.blue())
         emb.set_image(url = dog)
         await ctx.send(embed = emb)
 
@@ -31,28 +31,27 @@ class Animal(commands.Cog):
     async def cat(self, ctx):
         data = get(ckey).json()
         cat = data[0]['url']
-        emb = discord.Embed(title = 'Cats') 
+        emb = discord.Embed(title = 'Cats', color = discord.Color.blue())
         emb.set_image(url = cat)
         await ctx.send(embed = emb)
-    
+
 #### Command for Panda pics
 
     @commands.command()
     async def panda(self, ctx):
         data = get(pkey).json()
         panda = data['link']
-        emb = discord.Embed(title = 'Pandas')
+        emb = discord.Embed(title = 'Pandas', color = discord.Color.blue())
         emb.set_image(url = panda)
         await ctx.send(embed = emb)
-    
+
     @commands.command()
     async def rpanda(self, ctx):
         data = get(rpkey).json()
         panda = data['link']
-        emb = discord.Embed(title = 'Red Pandas')
+        emb = discord.Embed(title = 'Red Pandas', color = discord.Color.blue())
         emb.set_image(url = panda)
         await ctx.send(embed = emb)
-    
-def setup(bot):
-    bot.add_cog(Animal(bot)) 
 
+def setup(bot):
+    bot.add_cog(Animal(bot))
