9bdf6c3736c800c4c338a3c29b38f29b061a9897
E-M-P-E-R-O-R <70442265+E-M-P-E-R-O-R@users.noreply.github.com>
2021-07-26 20:31:12 +0530

    Update kitsu.py

--- src/kitsu.py
+++ src/kitsu.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import kitsupy
-from utilities import help_embed
 
 def getlist(dict):
     list = []
@@ -15,98 +14,103 @@ class Anime(commands.Cog):
 
     @commands.command()
     async def anime(self, ctx, *query):
-        if await help_embed(ctx.channel, "anime <anime_name>", query):
-            return
         query = ' '.join(query)
         data = kitsupy.search('anime', query)
-        id = getlist(data)
-        result = kitsupy.get_info('anime', id[1])
-        en_title, jp_title = result['titles']['en'], result['titles']['ja_jp']
-        en_jp_title = ''
-        if result['titles']['en_jp'] == '':
-            en_jp_title = en_title
+        if data['count'] == 0:
+            emb = discord.Embed(title='Anime', description= f'Sorry! No data on {query} found.', color = discord.Color.red())
+            await ctx.send(embed=emb)
         else:
-            en_jp_title = result['titles']['en_jp']
-        average_rating, popularity_rank, rating_rank = result['averageRating'], ['popularityRank'], result['ratingRank']
-        image = result['posterImage']['medium']
-        episode_count = result['episodeCount']
-        start_date = result['startDate']
-        end_date = result['endDate']
-        synopsis = result['synopsis']
+            id = getlist(data)
+            result = kitsupy.get_info('anime', id[1])
+            en_title, jp_title = result['titles']['en'], result['titles']['ja_jp']
+            en_jp_title = ''
+            if result['titles']['en_jp'] == '':
+                en_jp_title = en_title
+            else:
+                en_jp_title = result['titles']['en_jp']
+                average_rating, popularity_rank, rating_rank = result['averageRating'], ['popularityRank'], result['ratingRank']
+                image = result['posterImage']['medium']
+                episode_count = result['episodeCount']
+                start_date = result['startDate']
+                end_date = result['endDate']
+                synopsis = result['synopsis']
 
-        emb = discord.Embed(title = en_jp_title, description=synopsis, color = discord.Color.blue())
-        emb.add_field(name='English', value=en_title, inline=True)
-        emb.add_field(name='Japanese', value = jp_title, inline=True)
-        emb.add_field(name = chr(173), value = chr(173))
-        emb.add_field(name='Average Rating', value=average_rating, inline=True)
-        emb.add_field(name='Popularity Rank', value=popularity_rank, inline=True)
-        emb.add_field(name='Rating Rank', value=rating_rank, inline=True)
-        emb.add_field(name='Start Date', value=start_date, inline=True)
-        emb.add_field(name='End Date', value=end_date, inline=True)
-        emb.add_field(name='Episode Count', value=episode_count, inline=True)
-        emb.set_image(url = image)
-        await ctx.send(embed = emb)
+                emb = discord.Embed(title = en_jp_title, description=synopsis, color = discord.Color.blue())
+                emb.add_field(name='English', value=en_title, inline=True)
+                emb.add_field(name='Japanese', value = jp_title, inline=True)
+                emb.add_field(name = chr(173), value = chr(173))
+                emb.add_field(name='Average Rating', value=average_rating, inline=True)
+                emb.add_field(name='Popularity Rank', value=popularity_rank, inline=True)
+                emb.add_field(name='Rating Rank', value=rating_rank, inline=True)
+                emb.add_field(name='Start Date', value=start_date, inline=True)
+                emb.add_field(name='End Date', value=end_date, inline=True)
+                emb.add_field(name='Episode Count', value=episode_count, inline=True)
+                emb.set_image(url = image)
+                await ctx.send(embed = emb)
 
     @commands.command()
     async def manga(self, ctx, *query):
-        if await help_embed(ctx.channel, "manga <manga_name>", query):
-            return
         query = ' '.join(query)
-        data = kitsupy.search('anime', query)
-        id = getlist(data)
-        result = kitsupy.get_info('anime', id[1])
-        en_title, jp_title = result['titles']['en'], result['titles']['ja_jp']
-        en_jp_title = ''
-        if result['titles']['en_jp'] == '':
-            en_jp_title = en_title
+        data = kitsupy.search('manga', query)
+        if data['count'] == 0:
+            emb = discord.Embed(title='Manga', description= f'Sorry! No data on {query} found.', color = discord.Color.red())
+            await ctx.send(embed=emb)
         else:
-            en_jp_title = result['titles']['en_jp']
-        average_rating, popularity_rank, rating_rank = result['averageRating'], ['popularityRank'], result['ratingRank']
-        image = result['posterImage']['medium']
-        episode_count = result['episodeCount']
-        start_date = result['startDate']
-        end_date = result['endDate']
-        synopsis = result['synopsis']
-
-        emb = discord.Embed(title = en_jp_title, description=synopsis, color = discord.Color.blue())
-        emb.add_field(name='English', value=en_title, inline=True)
-        emb.add_field(name='Japanese', value = jp_title, inline=True)
-        emb.add_field(name = chr(173), value = chr(173))
-        emb.add_field(name='Average Rating', value=average_rating, inline=True)
-        emb.add_field(name='Popularity Rank', value=popularity_rank, inline=True)
-        emb.add_field(name='Rating Rank', value=rating_rank, inline=True)
-        emb.add_field(name='Start Date', value=start_date, inline=True)
-        emb.add_field(name='End Date', value=end_date, inline=True)
-        emb.add_field(name='Episode Count', value=episode_count, inline=True)
-        emb.set_image(url = image)
-        await ctx.send(embed = emb)
+            id = getlist(data)
+            result = kitsupy.get_info('manga', id[1])
+            en_title, jp_title = result['titles']['en'], result['titles']['ja_jp']
+            en_jp_title = ''
+            if result['titles']['en_jp'] == '':
+                en_jp_title = en_title
+            else:
+                en_jp_title = result['titles']['en_jp']
+            average_rating, popularity_rank, rating_rank = result['averageRating'], ['popularityRank'], result['ratingRank']
+            image = result['posterImage']['medium']
+            chapter_count = result['chapterCount']
+            start_date = result['startDate']
+            end_date = result['endDate']
+            synopsis = result['synopsis']
+            emb = discord.Embed(title = en_jp_title, description=synopsis, color = discord.Color.blue())
+            emb.add_field(name='English', value=en_title, inline=True)
+            emb.add_field(name='Japanese', value = jp_title, inline=True)
+            emb.add_field(name = chr(173), value = chr(173))
+            emb.add_field(name='Average Rating', value=average_rating, inline=True)
+            emb.add_field(name='Popularity Rank', value=popularity_rank, inline=True)
+            emb.add_field(name='Rating Rank', value=rating_rank, inline=True)
+            emb.add_field(name='Start Date', value=start_date, inline=True)
+            emb.add_field(name='End Date', value=end_date, inline=True)
+            emb.add_field(name='Chapter Count', value=chapter_count, inline=True)
+            emb.set_image(url = image)
+            await ctx.send(embed = emb)
 
     @commands.command()
     async def char(self, ctx, *query):
-        if await help_embed(ctx.channel, "char <character_name>", query):
-            return
         query = ' '.join(query)
         data = kitsupy.search('characters', query)
-        id = id = getlist(data)
-        result = kitsupy.get_info('characters', id[1])
-        name = result['name']
-        eng_name = result['names']['en']
-        jap_name = result['names']['ja_jp']
-        desc = result['description']
-        u_desc = '%.4090s' % desc + '...'
-        image = result['image']['original']
-        other_name = ''
-        if len(result['otherNames']) != 0:
-            other_name = result['otherNames'][0]
+        if data['count'] == 0:
+            emb = discord.Embed(title='Character', description= f'Sorry! No data on {query} found.', color = discord.Color.red())
+            await ctx.send(embed=emb)
         else:
-            other_name = 'No other names'
+            id = id = getlist(data)
+            result = kitsupy.get_info('characters', id[1])
+            name = result['name']
+            eng_name = result['names']['en']
+            jap_name = result['names']['ja_jp']
+            desc = result['description']
+            u_desc = '%.4090s' % desc + '...'
+            image = result['image']['original']
+            other_name = ''
+            if len(result['otherNames']) != 0:
+                other_name = result['otherNames'][0]
+            else:
+                other_name = 'No other names'
 
-        emb = discord.Embed(title = name, description=u_desc, color = discord.Color.blue())
-        emb.add_field(name="English", value=eng_name, inline=True)
-        emb.add_field(name="Japanese", value=jap_name, inline=True)
-        emb.add_field(name='Other Names', value=other_name, inline=True)
-        emb.set_image(url = image)
-        await ctx.send(embed = emb)
+            emb = discord.Embed(title = name, description=u_desc, color = discord.Color.blue())
+            emb.add_field(name="English", value=eng_name, inline=True)
+            emb.add_field(name="Japanese", value=jap_name, inline=True)
+            emb.add_field(name='Other Names', value=other_name, inline=True)
+            emb.set_image(url = image)
+            await ctx.send(embed = emb)
 
 def setup(bot):
     bot.add_cog(Anime(bot))
